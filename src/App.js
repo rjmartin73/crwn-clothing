58e299dd499bc5110590556adef9a45a3532d881
Ryan Martin <rjmartin73@gmail.com>
2020-01-09 21:18:04 -0700

    added custom input and btn components for signin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop-page/shop.component'
+import  SingInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SingInAndSignUp} />
         </Switch>
       </Router>
 
